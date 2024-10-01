26a80c8602e8d5a5544f02cf2aa9eeee592c6755
Zibait-it <lucas.zibaitis@lightit.io>
2024-10-01 11:33:56 -0300

    feat: implement UI

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,25 @@
 module.exports = {
   content: ["./index.html", "./css/**/*.css"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        manrope: ["Manrope", "sans-serif"],
+        ubuntu: ["Ubuntu", "sans-serif"],
+      },
+      colors: {
+        primary: "#FCFCFC",
+        yellow: "#CA8A04",
+        light_yellow: "#FEF8E7",
+        green: "#16A34A",
+        light_green: "#F0FAF4",
+        red: "#DC2626",
+        light_red: "#FDE8E8",
+        orange: "#EA580C",
+        dark_grey: "#1C202E",
+        light_grey: "#90949F",
+        dark: "#00000066",
+      },
+    },
   },
   plugins: [],
 };
